6f1931acb3a47d4a1a46e9c0af89916fae778458
Lei Chen <leighcheng@gmail.com>
2013-08-16 16:39:23 -0700

    added callcenter name uniqueness valication

--- app/models/callcenter.rb
+++ app/models/callcenter.rb
@@ -1,5 +1,6 @@
 class Callcenter < ActiveRecord::Base
   attr_accessible :name, :networked, :priority
   validates :name, :presence => true
+  validates :name, :uniqueness => true
   validates :priority, :inclusion => 1..6
 end
