318d9b47ee95c1ba4170c7edcc85112d9470aee2
Lei Chen <leighcheng@gmail.com>
2013-08-16 22:44:46 -0700

    removed model level priority inclusion

--- app/models/callcenter.rb
+++ app/models/callcenter.rb
@@ -2,6 +2,5 @@ class Callcenter < ActiveRecord::Base
   attr_accessible :name, :networked, :priority
   validates :name, :presence => true
   validates :name, :uniqueness => true
-  validates :priority, :inclusion => 1..6
   has_many :outsidenumbers
 end
