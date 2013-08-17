827dfbd3b663e2124c3324c9212ef648deaaaffa
Lei Chen <leighcheng@gmail.com>
2013-08-17 10:28:31 -0700

    added ajax in outsidenumber post form

--- app/controllers/outsidenumbers_controller.rb
+++ app/controllers/outsidenumbers_controller.rb
@@ -2,17 +2,25 @@ class OutsidenumbersController < ApplicationController
   def create
   	@callcenter = Callcenter.find(params[:callcenter_id])
   	@outsidenumber = @callcenter.outsidenumbers.create!(params[:outsidenumber])
-  	redirect_to @callcenter
-	
-  	rescue ActiveRecord::RecordInvalid
+  	respond_to do |format|
+      format.html { redirect_to @callcenter }
+      format.js
+    end
+     
+    rescue ActiveRecord::RecordInvalid
   	  if not params[:outsidenumber]["dn"] == ""
 	  	  if not params[:outsidenumber]["dn"].match /^\d{3}-\d{3}-\d{4}$/
-	  	    flash[:notice] = params[:outsidenumber]["dn"] + ' is not correct format 999-999-9999!'  	  	
+	  	    flash[:notice] = params[:outsidenumber]["dn"] + ' is not correct format 999-999-9999!'  
+	  	    format.js { render :js=>'alert("bad format");' }	  	
 	  	  else
 	  	    flash[:notice] = params[:outsidenumber]["dn"] + ' has been taken!'
+	  	    format.js { render :js=>'alert("number taken");' }	
 	  	  end
-  	  end
-      redirect_to @callcenter
+  	   end
+       redirect_to @callcenter 
+      
     end
+
+
 end
 
