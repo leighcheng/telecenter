83f6acbf9c7f0a290177b8089aa579793eaa4cf8
Lei Chen <leighcheng@gmail.com>
2013-08-24 12:00:06 -0700

    added validation to join table models

--- app/models/callcenter_msg.rb
+++ app/models/callcenter_msg.rb
@@ -2,4 +2,5 @@ class CallcenterMsg < ActiveRecord::Base
   attr_accessible :callcenter_id, :msg_id 
   belongs_to :callcenter
   belongs_to :msg
+  validates_uniqueness_of :msg_id, scope: :callcenter_id 
 end
--- app/models/callcenter_vsvm.rb
+++ app/models/callcenter_vsvm.rb
@@ -2,4 +2,5 @@ class CallcenterVsvm < ActiveRecord::Base
   attr_accessible :callcenter_id, :vsvm_id
   belongs_to :callcenter
   belongs_to :vsvm
+  validates_uniqueness_of :vsvm_id, scope: :callcenter_id 
 end
