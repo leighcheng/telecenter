ef19ec7062a7ae4a23849c98892cb0b28c9a50af
Lei Chen <leighcheng@gmail.com>
2013-09-16 15:00:37 -0700

    deleting

--- db/create_masterview.sql
+++
@@ -1,5 +0,0 @@
-﻿create or replace view masterview as
-select callcenters.name, callcenters.priority, callcenters.networked, cdns.extension, outsidenumbers.dn 
-from callcenters 
-left outer join cdns on callcenters.id = cdns.callcenter_id
-left outer join outsidenumbers on callcenters.id = outsidenumbers.callcenter_id
